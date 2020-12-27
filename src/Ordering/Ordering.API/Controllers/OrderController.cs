01d155d2941e6e4382010df801a1353c186a5b11
Asif <asif.mahmud@selise.ch>
2020-12-28 01:48:54 +0600

    Section -5 [ DONE ]

--- src/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +14,29 @@ namespace Ordering.API.Controllers
     [Route("[controller]")]
     public class OrderController : ControllerBase
     {
-        
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrderByUserNameQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        // Testing purpose
+        [HttpPost]
+        public async Task<ActionResult<OrderResponse>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
     }
 }
  
